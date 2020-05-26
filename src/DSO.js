456ae94cc5c337d1cbdc7729adc88692a275451a
Paul August <paul.augustp@gmail.com>
2020-05-26 20:55:06 +0100

    Added dynamic date and completed DSO page functionality

--- src/DSO.js
+++ src/DSO.js
@@ -4,31 +4,55 @@ import { Link } from 'react-router-dom';
 class Dso extends Component {
 constructor(props){
   super(props);
-  this.state = "";
+  this.state = { ArBal: "", CrSales: "", days: "", dso: "" }; 
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.calcDso = this.calcDso.bind(this);
+};
+
+handleChange(e) {
+  this.setState({ [e.target.name] : e.target.value });
 }
+
+calcDso(){
+  let dsoValue =  Math.round(this.state.ArBal / this.state.CrSales * this.state.days);
+  this.setState({dso: dsoValue});
+}
+
+handleSubmit(event) {
+  this.calcDso()
+  event.preventDefault();
+}
+
+
+
 render(){
   return (
+    <form onSubmit={this.handleSubmit}>
 <div className="main-container">
-{/* <Header /> */}
   <div className="column">
     <div className="col-sm-auto">
       <div className="col-md-auto flex justify-content-center">
         <h1>Days Sales Outstanding</h1>
-
-        {/* <form action="/dso" method="post"> */}
+    
           <div className="variable-input">
             <p class="description-text">Enter your total receivables balance</p>
-            <input type="number" name="Ar_bal" placeholder="AR Balance" />
+            <label>
+            <input type="number" name="ArBal" value={this.state.ArBal} onChange={this.handleChange} placeholder="AR Balance"/>
+            </label>
           </div>
 
           <div className="variable-input">
             <p className="description-text">Enter your current credit sales</p>
-            <input type="number" name="credit_sales" placeholder="Credit Sales" />
+            <label>
+            <input type="number" name="CrSales" value={this.state.CrSales} onChange={this.handleChange} placeholder="Credit Sales" />
+            </label>
           </div>
 
           <div className="variable-select">
             <p className="description-text">Select the number of days in current month</p>
-            <select type="number" name="days">
+            <select type="number" name="days" value={this.state.days} onChange={this.handleChange}>
               <option value="31">31</option>
               <option value="30">30</option>
               <option value="29">29</option>
@@ -36,7 +60,7 @@ render(){
             </select>
           </div>
         <div className="button">
-        <button className="btn btn-success btn-sm" type="submit" name="submit">Calculate DSO </button>
+        <button className="btn btn-success btn-sm" type="submit" name="submit" onClick={this.calcDso}>Calculate DSO </button>
         </div>
         <div className="button">
           <Link to="/">
@@ -44,13 +68,14 @@ render(){
           </Link>
         </div>
 
-         {/* <form/> */}
-
+        <div className="result">
+          <p name="dso">Your DSO result is: {this.state.dso}</p>
+        </div>
       </div>
     </div>
   </div>
-  {/* <Footer /> */}
 </div>
+</form>
       );
   }
 }
--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className="footer-padding">
   <div className="footer">
-    <p>Made by Paul August. 2019 </p>
+    <p>Made by Paul August. &copy; {(new Date().getFullYear())} </p>
   </div>
 </div>
     )
