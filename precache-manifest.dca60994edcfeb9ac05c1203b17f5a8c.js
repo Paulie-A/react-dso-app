d505cb0867a381c2291ed75bfce76942fefbfa9d
Paul August <paul.augustp@gmail.com>
2020-06-08 21:36:42 +0100

    Updates

---
+++ precache-manifest.dca60994edcfeb9ac05c1203b17f5a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f968c995870ef41c4e55226a37e76d5e",
+    "url": "//paulie-a.github.io/react-dso-app/index.html"
+  },
+  {
+    "revision": "7b50f459fb298778879b",
+    "url": "//paulie-a.github.io/react-dso-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "999b802c5633983a9733",
+    "url": "//paulie-a.github.io/react-dso-app/static/css/main.55fcac45.chunk.css"
+  },
+  {
+    "revision": "7b50f459fb298778879b",
+    "url": "//paulie-a.github.io/react-dso-app/static/js/2.0569bec3.chunk.js"
+  },
+  {
+    "revision": "999b802c5633983a9733",
+    "url": "//paulie-a.github.io/react-dso-app/static/js/main.31451725.chunk.js"
+  },
+  {
+    "revision": "32942a787bb336f7172a",
+    "url": "//paulie-a.github.io/react-dso-app/static/js/runtime-main.e7f2bfc0.js"
+  }
+]);
