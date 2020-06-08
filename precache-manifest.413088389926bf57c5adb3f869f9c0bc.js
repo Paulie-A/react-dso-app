a8223ed119b919bdca05e77d05dbc6d9168d231f
Paul August <paul.augustp@gmail.com>
2020-06-08 22:09:33 +0100

    Updates

--- precache-manifest.413088389926bf57c5adb3f869f9c0bc.js
+++ precache-manifest.413088389926bf57c5adb3f869f9c0bc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c238a825f2244033e6a1c6ae5ba2404",
+    "revision": "429d32ea9690bcb6dd559f3c876cbf23",
     "url": "//paulie-a.github.io/react-dso-app/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "//paulie-a.github.io/react-dso-app/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "a1a05eabf0cc826b882f",
+    "revision": "d8d55290cc56561e03bb",
     "url": "//paulie-a.github.io/react-dso-app/static/css/main.55fcac45.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "//paulie-a.github.io/react-dso-app/static/js/2.0569bec3.chunk.js"
   },
   {
-    "revision": "a1a05eabf0cc826b882f",
-    "url": "//paulie-a.github.io/react-dso-app/static/js/main.134638a1.chunk.js"
+    "revision": "d8d55290cc56561e03bb",
+    "url": "//paulie-a.github.io/react-dso-app/static/js/main.f66eddd1.chunk.js"
   },
   {
     "revision": "32942a787bb336f7172a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//paulie-a.github.io/react-dso-app/precache-manifest.8ec024c57b40cb4587e75fed6c348db4.js"
+  "//paulie-a.github.io/react-dso-app/precache-manifest.413088389926bf57c5adb3f869f9c0bc.js"
 );
 
 self.addEventListener('message', (event) => {
