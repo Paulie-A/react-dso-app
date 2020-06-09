0c5de45d97258373f0c4771dfbc4a7fe04be0cb7
Paul August <paul.augustp@gmail.com>
2020-06-09 23:17:59 +0100

    testing HasRouter code

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
     <HashRouter basename="/">
       <Header />
       <Switch>
-     {/* <Route exact path='/' component={Home} /> */}
-     <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
-     {/* <Route exact path='/dso' component={Dso} /> */}
-     <Route exact path={process.env.PUBLIC_URL + '/dso'} component={Dso} />
-     {/* <Route exact path='/cei' component={Cei} /> */}
-     <Route exact path={process.env.PUBLIC_URL + '/cei'} component={Cei} />
+     <Route exact path='/' component={Home} />
+     {/* <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} /> */}
+     <Route exact path='/dso' component={Dso} />
+     {/* <Route exact path={process.env.PUBLIC_URL + '/dso'} component={Dso} /> */}
+     <Route exact path='/cei' component={Cei} />
+     {/* <Route exact path={process.env.PUBLIC_URL + '/cei'} component={Cei} /> */}
    </Switch>
 <Footer />
 </HashRouter>
